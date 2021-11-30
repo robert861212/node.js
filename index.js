fbcca2f413bb00817f933bc1f4dc880c0ec675bf
robert861212 <robert861212@gmail.com>
2021-11-30 02:21:48 -0500

    refactor

--- index.js
+++ index.js
@@ -15,10 +15,9 @@ app.get('/', function(request, response) {
 	response.header("Access-Control-Allow-Origin", "*");
     response.header("Access-Control-Allow-Headers", "X-Requested-With");
     response.set('Content-Type', 'text/html');
-    response.send("Hello World");
-    // fs.readFile("index.html", 'utf8', function (err,data) {
-    // response.send(data);
-    // });
+    fs.readFile("index.html", 'utf8', function (err,data) {
+    response.send(data);
+    });
 });
 
 
--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
-1. npm i
-2. npm start
-3. go to localhost:3000
+The repo is a starting point for anyone who wants to develop a node.js server.
+
+1. install npm
+2. Command: npm i
+2. Command: npm start
+3. go to localhost:3000 to review the dummy website hosted by the server
